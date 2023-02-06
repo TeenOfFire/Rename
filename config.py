14b12f9061128ce01b91cacbbdfe39b4eb10e4ca
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-02-06 09:27:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6159823074:AAG-lCFVF6-7UfJyhKJOev5_RF-EUkwmXds")
     OWNER_ID = int(os.environ.get("OWNER_ID", 1058015838))
     AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", None)
-    DATABASE_URI = os.environ.get("DATABASE_URI", mongodb+srv://Testing:Testing@cluster0.fkelfqj.mongodb.net/?retryWrites=true&w=majority)
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://Testing:Testing@cluster0.fkelfqj.mongodb.net/?retryWrites=true&w=majority")
