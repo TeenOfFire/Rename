10ec975968f7fc9641699964144dcb1ee18780fd
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-02-06 15:56:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     BANNED_USERS = []
     DOWNLOAD_LOCATION = "./DOWNLOADS" 
     API_ID = int(os.environ.get("API_ID", "1736204"))
-    API_HASH = os.environ.get("API_HASH" "890d40e0f91a4de32dec2965444b2cbe")
+    API_HASH = os.environ.get("API_HASH", "890d40e0f91a4de32dec2965444b2cbe")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6159823074:AAG-lCFVF6-7UfJyhKJOev5_RF-EUkwmXds")
     OWNER_ID = int(os.environ.get("OWNER_ID", "1058015838"))
     AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", None)
